56ce6f5ee21e956857561f48a471ef014457716c
Brightened Mind <brightenedmindcorporation@gmail.com>
2026-07-25 10:47:14 +0200

    Fix routing and redirects

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
+        // Redirige tout lien contenant /academy/app/... vers /academy/...
         source: "/academy/app/:path*",
         destination: "/academy/:path*",
         permanent: true,
