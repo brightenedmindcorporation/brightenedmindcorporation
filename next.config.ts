1767cddf10bab1c0bcb7fe648efe8765b31626f1
Brightened Mind <brightenedmindcorporation@gmail.com>
2026-07-25 10:41:01 +0200

    Add redirection rules in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/academy/app/:path*",
+        destination: "/academy/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
